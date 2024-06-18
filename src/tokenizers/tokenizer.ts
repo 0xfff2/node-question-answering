f7c7f55faaa7e8c447ddc12b35d5e8a50d2b652f
0xfff2 <169174981+0xfff2@users.noreply.github.com>
2024-06-18 14:13:17 -0300

    Update tokenizer.ts

--- src/tokenizers/tokenizer.ts
+++ src/tokenizers/tokenizer.ts
@@ -9,25 +9,10 @@ import {
 import { ModelType } from "../models";
 
 export interface TokenizerBaseOptions {
-  /**
-   * @default true
-   */
   lowercase?: boolean;
-  /**
-   * Name of the merges file (if applicable to the tokenizer)
-   * @default "merges.txt"
-   */
   mergesFile?: string;
-  /**
-   * Directory under which the files needed by the tokenizer are located.
-   * Must be an absolute path.
-   */
   filesDir: string;
   modelType: ModelType;
-  /**
-   * Name of the vocab file (if applicable to the tokenizer)
-   * @default "vocab.txt" | "vocab.json"
-   */
   vocabFile?: string;
 }
 
@@ -41,14 +26,9 @@ export abstract class Tokenizer<T extends BaseTokenizer<object> = BaseTokenizer<
 
   abstract getContextStartIndex(encoding: Encoding): number;
 
-  /**
-   * Get the last index of the context of an encoding
-   * @param encoding Encoding for which to return last context index
-   * @virtual
-   */
   getContextEndIndex(encoding: Encoding): number {
-    const nbAddedTokens = encoding.specialTokensMask.reduce((acc, val) => acc + val, 0);
-    const actualLength = encoding.length - nbAddedTokens;
+    const nbAddedTokens = encoding.getSpecialTokensMask().reduce((acc, val) => acc + val, 0);
+    const actualLength = encoding.getLength() - nbAddedTokens;
     const contextLength = actualLength - this.getQuestionLength(encoding);
 
     return this.getContextStartIndex(encoding) + contextLength - 1;
@@ -58,19 +38,8 @@ export abstract class Tokenizer<T extends BaseTokenizer<object> = BaseTokenizer<
     return this.tokenizer.encode(sequence, pair, { addSpecialTokens });
   }
 
-  /**
-   * Enable/change padding with specified options
-   * @param maxLength Padding length
-   * @virtual
-   */
   setPadding(maxLength: number): Readonly<PaddingConfiguration> {
     return this.tokenizer.setPadding({ maxLength });
   }
 
-  setTruncation(
-    maxLength: number,
-    options?: TruncationOptions
-  ): Readonly<TruncationConfiguration> {
-    return this.tokenizer.setTruncation(maxLength, options);
-  }
-}
+  setTr
